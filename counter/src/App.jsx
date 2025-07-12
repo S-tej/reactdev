8d83be8c14c86c3f849c06aace7082ab15628e36
S-tej <swarantejvajrapu8055@gmail.com>
2025-07-13 00:50:35 +0530

    currency converter app

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,11 +5,18 @@ function App() {
   
   // let count = 5;
   let [count,setCount] =useState(5);
+  let [count2,setCount2] =useState(6);
 
   const add = ()=>{
     // count+=1
     if(count===20) return 
-    setCount(count+1)
+    setCount(count2)
+    setCount2(count)
+    // setCount(count=>count+1)
+    // setCount(count=>count+1)
+    // setCount(count=>count+1)
+    // setCount(count=>count+1)
+    // setCount(count=>count+1)
     console.log("added",count)
   }
 
@@ -25,7 +32,7 @@ function App() {
   return (
     <>
       <h1>This is a counter</h1>
-      <h3>counter value : {count}</h3>
+      <h3>counter value : {count} {count2}</h3>
       <br />
       <button onClick={add}>Add value {count}</button>
       <br />
